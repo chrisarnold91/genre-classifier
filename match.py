10d5a52c0f21c7923b66f1b4194c3da529df45e5
Chris <chrisarnold391@gmail.com>
2017-02-03 15:29:27 -0500

    change table keys to pitches from timestamps, add more training audio tracks (add pop genre)

--- match.py
+++ match.py
@@ -1,35 +1,43 @@
-import numpy as np
+# import numpy as np
 import sys
 import pickle
 import pprint
+import warnings
 
 from madmom.utils.midi import *
+from os import listdir
 from populate import *
 
 TABLE_FILE = 'table.csv'
-TABLE = {}
 TALLY = {}
 
 def main():
-    midi_train(TABLE, str(sys.argv[1]))
-    match(pickle.load(open("table.p", "rb")))
-    
-def match(master_table):
-    for tick, trial_pair in TABLE.items():
-        if tick in master_table.keys():
-            for master_pair in master_table[tick]:
-                if trial_pair[0][0] == master_pair[0]:
-                    record_match(trial_pair[0][1])
-    
-    print TALLY
-    guess = max(TALLY, key=TALLY.get)
-    print 'is this {}?'.format(guess)
+    warnings.filterwarnings('ignore')
 
-def record_match(file):
-    if file not in TALLY.keys():
-        TALLY[file] = 1
+    master_table = pickle.load(open("table.p", "rb"))
+
+    for file in listdir('test-set'):
+        if not file.startswith('.'):
+            table = {}
+            midi_train(table, 'test-set/' + file)
+            match(file, table, master_table)
+
+    pprint.pprint(TALLY)
+    
+def match(file, table, master_table):
+    TALLY[file] = {}
+
+    for pitch, trial_pair in table.items():
+        if pitch in master_table.keys():
+            for master_pair in master_table[pitch]:
+                if trial_pair[0][0] == master_pair[0]:
+                    record_match(file, master_pair[1])
+
+def record_match(file, genre):
+    if genre not in TALLY[file].keys():
+        TALLY[file][genre] = 1
     else:
-        TALLY[file] += 1
+        TALLY[file][genre] += 1
 
 if __name__ == '__main__':
     main()
--- populate.py
+++ populate.py
@@ -9,16 +9,16 @@ from os import listdir
 
 
 TABLE_FILE = 'table.csv'
-CATEGORIES = ['midi-classical', 'midi-rock', 'midi-videogame']
+GENRES = ['midi-classical', 'midi-rock', 'midi-pop']
 SEPARATOR = ','
+
+TABLE = {}
 
 ONSET = 0
 PITCH = 1
 DURATION = 2
 VELOCITY = 3
 CHANNEL = 4
-
-TABLE = {}
 
 
 def main():
@@ -28,20 +28,24 @@ def main():
     np.set_printoptions(threshold=np.nan)
     # np.set_printoptions(edgeitems=10)
 
-    for category in CATEGORIES:
-        for file in listdir(category):
+    for genre in GENRES:
+        for file in listdir(genre):
             if not file.startswith('.'):
-                midi_train(TABLE, file, category + '/')
+                midi_train(TABLE, file, genre + '/')
 
     pprint.pprint(TABLE)
     pickle.dump(TABLE, open("table.p", "wb"))
     export_table()
 
-def midi_train(table, file, category=""):
-    if category != "":
-        print category + file
+def midi_train(table, file, genre=""):
+    midi = MIDIFile.from_file(genre + file)
 
-    midi = MIDIFile.from_file(category + file)
+    if genre != "":
+        genre_trimmed = genre.split("-")[1][:-1]
+        print genre + file
+
+    else:
+        genre_trimmed = "???"
 
     # first matrix column is the offset in ticks (milliseconds)
     notes = midi.notes(unit='ticks')
@@ -54,14 +58,17 @@ def midi_train(table, file, category=""):
     events = midi.tracks[0].events
     event_index = find_time_signature(events)
     if event_index == None:
-        print 'Time Signature not found'
+        print 'Time Signature not found for {}'.format(file)
         return
 
     beats_per_bar = events[event_index].data[ONSET]
     ticks_per_bar = beats_per_bar * midi.resolution
 
     # wow the units check out!
-    bars = beats / beats_per_bar
+    # bars = beats / beats_per_bar
+
+    # remember current onset and its highest pitch so far
+    curr = (notes[0][ONSET], notes[0][PITCH])
 
     for note in range(len(notes)):
         onset = notes[note][ONSET]
@@ -70,35 +77,34 @@ def midi_train(table, file, category=""):
         # is this note at the beginning of the bar?
         if onset % ticks_per_bar == 0:
 
-            if onset not in table.keys():
-                table[onset] = []
-                table[onset].append((pitch, file))
+            # does this note have the same offset?
+            if onset == curr[ONSET]:
 
+                # look for the highest pitch for this onset
+                if pitch > curr[PITCH]:
+                    curr = (onset, pitch)
+
+            # we've reached the next onset
             else:
-                highest = get_highest_pitch(table[onset], file)
-                set_pitch(table[onset], file, pitch, highest)
+                add_note(table, genre_trimmed, curr)
+                curr = (onset, pitch)
+
+    add_note(table, genre_trimmed, curr)
+
+def add_note(table, genre_trimmed, curr):
+    # add to table if it's a new pitch
+    if curr[PITCH] not in table.keys():
+        table[curr[PITCH]] = [(curr[ONSET], genre_trimmed)]
+
+    # append if pitch already exists as a key
+    else:
+        table[curr[PITCH]].append((curr[ONSET], genre_trimmed))
 
 def find_time_signature(events):
     for i in range(len(events)):
-        if type(events[i]) == TimeSignatureEvent:
+        if isinstance(events[i], TimeSignatureEvent):
             return i
     return None
-
-def get_highest_pitch(row, file):
-    for tup in row:
-        if tup[1] == file:
-            return tup[0]
-    return None
-
-def set_pitch(row, file, pitch, highest):
-    for tup in row:
-        if tup[1] == file:
-            # found new highest pitch?
-            if pitch > highest:
-                row.remove(tup)
-                row.append((pitch, file))
-            return
-    row.append((pitch, file))
 
 def export_table():
     file = open(TABLE_FILE, 'w')
@@ -112,3 +118,22 @@ def export_table():
 
 if __name__ == '__main__':
     main()
+
+
+#########################################################
+# pitch references                                      #
+#########################################################
+# octave|c  |c# |d  |d# |e  |f  |f# |g  |g# |a  |a# |b  # 
+#########################################################
+#   0   |0  |1  |2  |3  |4  |5  |6  |7  |8  |9  |10 |11 #
+#   1   |12 |13 |14 |15 |16 |17 |18 |19 |20 |21 |22 |23 #
+#   2   |24 |25 |26 |27 |28 |29 |30 |31 |32 |33 |34 |35 #
+#   3   |36 |37 |38 |39 |40 |41 |42 |43 |44 |45 |46 |47 #
+#   4   |48 |49 |50 |51 |52 |53 |54 |55 |56 |57 |58 |59 #
+#   5   |60 |61 |62 |63 |64 |65 |66 |67 |68 |69 |70 |71 #
+#   6   |72 |73 |74 |75 |76 |77 |78 |79 |80 |81 |82 |83 #
+#   7   |84 |85 |86 |87 |88 |89 |90 |91 |92 |93 |94 |95 #
+#   8   |96 |97 |98 |99 |100|101|102|103|104|105|106|107#
+#   9   |108|109|110|111|112|113|114|115|116|117|118|119#
+#   10  |120|121|122|123|124|125|126|127|   |   |   |   #
+#########################################################
